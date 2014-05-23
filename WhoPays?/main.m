610d1fafb71ec57262ef004c8334b2b1e6c4e48a
Malcolm Crum <malcolm.crum@sonos.com>
2014-05-23 11:30:07 -0700

    Initial Commit

---
+++ WhoPays?/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WhoPays?
+//
+//  Created by sonossqa on 5/23/14.
+//  Copyright (c) 2014 Malcolm Crum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WPAppDelegate class]));
+    }
+}
